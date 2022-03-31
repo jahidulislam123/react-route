33bca4049ed49a23524594a3942d2390f5b386ed
unknown <islam15-3879@diu.edu.bd>
2022-03-31 20:06:24 +0600

    route-id

--- src/components/Friends/Friends.js
+++ src/components/Friends/Friends.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Friend from '../Friend/Friend';
 
 const Friends = () => {
+    const[friends,setFriends]=useState([]);
+    useEffect(()=>{
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res=>res.json())
+        .then(data=>setFriends(data));
+    },[])
     return (
         <div>
             <h3>hello friends how are you</h3>
-            <p><small>Parle kico taka dar dis </small></p>
+            <p><small>Parle kico taka dar dis {friends.length} </small></p>
+            {
+                friends.map(friend=> <Friend key={friend.id} friend={friend} ></Friend> )
+            }
         </div>
     );
 };
