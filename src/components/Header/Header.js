450565022e6974d47493187f65a29c62e76e1302
unknown <islam15-3879@diu.edu.bd>
2022-03-31 22:12:04 +0600

    added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import CustomLink from '../CustomLink/CustomLink';
+
+const Header = () => {
+    return (
+        <div>
+        
+        {/* hii */}
+                  <h1>Welcome to my Routing</h1>
+                  <nav>
+                      <CustomLink to="/home">Home</CustomLink>
+                          <CustomLink to="/friends">Friends</CustomLink>
+                          <CustomLink to="/about">About</CustomLink>
+                  </nav>
+
+        </div>
+    );
+};
+
+export default Header;
