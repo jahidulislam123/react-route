3a47e8d622c5f368d2f7a74c91ba6a024f788413
unknown <islam15-3879@diu.edu.bd>
2022-03-31 18:42:43 +0600

    first commit

---
+++ src/components/Friends/Friends.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Friends = () => {
+    return (
+        <div>
+            <h3>hello friends how are you</h3>
+            <p><small>Parle kico taka dar dis </small></p>
+        </div>
+    );
+};
+
+export default Friends;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h3>This is home</h3>
+            <h4>place for home </h4>
+        </div>
+    );
+};
+
+export default Home;
