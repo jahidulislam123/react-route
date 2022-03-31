3ff1063ab5694a5a4a893e89556fe9b212776a9d
unknown <islam15-3879@diu.edu.bd>
2022-03-31 19:09:40 +0600

    route-added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import logo from './logo.svg';
+import {  Route, Routes } from 'react-router-dom';
 import './App.css';
-import Home from './components/Home/Home';
-import Friends from './components/Friends/Friends';
 import About from './components/About/About';
+import Friends from './components/Friends/Friends';
+import Home from './components/Home/Home';
+import NotFound from './components/NotFound/NotFound';
+
 
 function App() {
   return (
     <div className="App">
-     <Home></Home>
-     <Friends></Friends>
-     <About>
-       
-     </About>
+      <h1>Welcome to my fancy Routing website</h1>
+     <Routes>
+        <Route path="/" element={<Home></Home>} ></Route>
+        <Route path="/friends" element={<Friends></Friends>} ></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
+     </Routes>
     </div>
   );
 }
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h3>The page you are looking are not found</h3>
+        </div>
+    );
+};
+
+export default NotFound;
